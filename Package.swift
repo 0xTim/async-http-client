f27de099f050b55c35c0aa3a8412657e0fd7db5b
Tim Condon <0xTim@users.noreply.github.com>
2020-12-16 15:41:41 +0000

    Remove .git from the manifest

--- Package.swift
+++ Package.swift
@@ -21,11 +21,11 @@ let package = Package(
         .library(name: "AsyncHTTPClient", targets: ["AsyncHTTPClient"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.19.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.8.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.5.1"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+        .package(url: "https://github.com/apple/swift-nio", from: "2.19.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl", from: "2.8.0"),
+        .package(url: "https://github.com/apple/swift-nio-extras", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services", from: "1.5.1"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.4.0"),
     ],
     targets: [
         .target(
